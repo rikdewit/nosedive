1dfede2e58973ff1d6f458d87a827152d74866f2
rik <rik@bada.colo.transip.net>
2020-03-18 22:53:08 +0100

    star ratings update the ranking

--- main/models.py
+++ main/models.py
@@ -6,6 +6,7 @@ from django.dispatch import receiver
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     rating = models.FloatField(default=3)
+    times_rated = models.IntegerField(default=0)
 
     def __str__(self):
         return self.user.username
--- main/views.py
+++ main/views.py
@@ -21,22 +21,26 @@ class UserListView(TemplateView):
 
     def post(self,request, **kwargs):
         form = StarsForm(self.request.POST)
-        print(form.data)
+        data = form.data
+        print(data)
         if form.is_valid():
             print("form redirect!")
-            return redirect('/main/')
         else:
-            print(form.data)
             print("invalid form")
 
-        
-        #rated_user = User.objects.get(username=request.user.username)
-        #given_rating = form.data['rating']
-        #current_rating = rated_user.profile.rating
-        #my_rating = request.user.profile.rating
-        #new_rating = current_rating-(my_rating/5)*(current_rating - given_rating)
-        #rated_user.profile.rating = new_rating
-       # rated_user.save()
+        rated_user = User.objects.get(username=data['rated_user'])
+        given_rating = float(data['stars'])
+
+        print(rated_user)
+        current_rating = rated_user.profile.rating
+        print(current_rating)
+        times_rated = rated_user.profile.times_rated
+
+        my_rating = request.user.profile.rating
+        new_rating = current_rating-(my_rating/5)*(current_rating - given_rating)*(1/(times_rated+1))
+        rated_user.profile.rating = new_rating
+        rated_user.profile.times_rated += 1
+        rated_user.save()
         return redirect('/main/')
 
 class RateView(View):
