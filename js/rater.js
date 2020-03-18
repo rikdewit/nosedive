de775ddf026d6f089173c62425c2ba6deb2508d3
rik <rik@bada.colo.transip.net>
2020-03-18 14:10:21 +0100

    made rater a component

---
+++ js/rater.js
@@ -0,0 +1,28 @@
+export function Rater(ratingElement) {
+    const stars = ratingElement.querySelectorAll(".star");
+
+
+    for (let i = 0; i < stars.length; i++) {
+        const star = stars[i];
+
+
+        star.addEventListener("mouseenter", function (event) {
+
+            for (let j = 0; j <= i; j++) {
+                stars[j].classList.toggle("selecting")
+            }
+
+            console.log(event.type)
+            console.log("star" + String(i))
+            console.log(selected)
+
+        });
+        star.addEventListener("mouseleave", function (event) {
+
+            for (let j = 0; j < stars.length; j++) {
+                stars[j].classList.remove("selecting")
+            }
+        });
+    }
+
+}
